3fd0471ddb9369cafdf1ac7b4149bd25bf9d0380
KeeganJennings <34640514+KeeganJennings@users.noreply.github.com>
2019-02-10 17:05:30 -0600

    AIrplane Test Engine Started String and Take Off

--- Sprint0AerialVehicle/Sprint1AerialVehicleUnitTest/UnitTestAirplane.cs
+++ Sprint0AerialVehicle/Sprint1AerialVehicleUnitTest/UnitTestAirplane.cs
@@ -23,5 +23,38 @@ namespace Sprint1AerialVehicleUnitTest
             Assert.AreEqual($"This {airplane} has a max altitude of {airplane.MaxAltitude} ft.\n It's current altitude is {airplane.CurrentAltitude} ft.\n {airplane.engine} is not started", airplane.About());
 
         }
+
+        [TestMethod]
+        public void TestAirplaneGetEngineStartedString()
+        {
+            //Arrange
+            airplane = new Airplane();
+
+            //Act
+            string engineStatusBefore = airplane.GetEngineStartedString();
+            airplane.engine.isStarted = true;
+            string engineStatureAfter = airplane.GetEngineStartedString();
+
+            //Assert
+            Assert.AreEqual($"{airplane} is no longer flying", engineStatusBefore);
+            Assert.AreEqual($"{airplane} is flying", engineStatureAfter);
+        }
+
+        [TestMethod]
+        public void TestAirplaneTakeOff()
+        {
+            //Arrange
+            airplane = new Airplane();
+
+            //Act
+            string canTakeOffBefore = airplane.TakeOff();
+            airplane.engine.isStarted = true;
+            string canTakeOffAfter = airplane.TakeOff();
+
+            //Assert
+
+            Assert.AreEqual($"{airplane} cannot take off because it's engine is not started", canTakeOffBefore);
+            Assert.AreEqual("", canTakeOffAfter);
+        }
     }
 }
